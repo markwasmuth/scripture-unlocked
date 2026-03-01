8c5e0be55c95cc70561d1be7222bc7ed228430cb
Mark Wasmuth <mark@scriptureunlocked.app>
2026-03-01 04:06:01 -0500

    Fix capacitor config for web-only build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,3 +1,3 @@
-﻿import type { CapacitorConfig } from "@capacitor/cli";
-const config: CapacitorConfig = { appId: "com.scriptureunlocked.app", appName: "Scripture Unlocked", webDir: "out", server: { androidScheme: "https" }, plugins: { SplashScreen: { launchShowDuration: 2000, backgroundColor: "#14142E", showSpinner: false }, StatusBar: { style: "DARK", backgroundColor: "#14142E" } } };
+// Capacitor config - used only for mobile builds
+const config = { appId: 'com.scriptureunlocked.app', appName: 'Scripture Unlocked', webDir: 'out' };
 export default config;
